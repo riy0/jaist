e4a8bb56794210171847052f840d6f3d690e79a6
riy0 <chac0ptx@gmail.com>
2019-04-22 22:43:25 +0900

    changed ex1

--- gameinfo/lesson2.py
+++ gameinfo/lesson2.py
@@ -5,54 +5,62 @@ from numpy.random import *
 
 SIZE = 20
 DATA = 2
-t_point=[]
-f_point=[]
-cnt= DATA
-
-for i in range(DATA):
-    t_point.append([randint(0,9),randint(0,19)])
-    f_point.append([randint(10,19),randint(0,19)])
-
 ok = 'o '
 out= 'x '
+total = 0
 
-def decide_shortest_point(x,y):
-    min_t = 100000000
-    min_f = 100000000
+def decide_shortest_point(array,t_point, f_point, x,y):
+    min_t = 100000
+    min_f = 100000
     for i in range(DATA):
-        tx = t_point[i][0] - x
-        ty = t_point[i][1] - y
-        dist = tx * tx + ty * ty
-        if dist < min_t:
-            min_t = dist
-
-    for i in range(DATA):
-        fx = f_point[i][0] - x
-        fy = f_point[i][1] - y
-        dist = fx * fx + fy * fy
-        if dist < min_f:
-            min_f = dist
+        t = (t_point[i][0] - x) ** 2 + (t_point[i][1] - y) **2
+        f = (f_point[i][0] - x) ** 2 + (f_point[i][1] - y) **2
+        if t < min_t:
+            min_t = t
+        if f < min_f:
+            min_f = f
 
     if min_f < min_t:
         array[x][y] = out
     else:
         array[x][y] = ok
+    return array
 
-array = np.full((SIZE, SIZE),'. ')
+def execute(): 
+    #初期化
+    t_point = []
+    f_point = []
+    t_cnt = DATA
+    f_cnt = DATA 
+    array = np.full((SIZE, SIZE),'. ')
 
-for i in range(DATA):
-    array[t_point[i][0]][t_point[i][1]] = '@ '
-    array[f_point[i][0]][f_point[i][1]] = 'X '
+    for i in range(DATA):
+        t_point.append([randint(0,9),randint(0,19)])
+        f_point.append([randint(10,19),randint(0,19)])
+
+    for i in range(DATA):
+        array[t_point[i][0]][t_point[i][1]] = '@ '
+        array[f_point[i][0]][f_point[i][1]] = 'X '
 
 
-for j in range(SIZE):
-    for i in range(SIZE):
-        if array[i][j] == '. ':
-            decide_shortest_point(i,j)
-        sys.stdout.write(array[i][j])
-        if array[i][j] == ok:
-           cnt = cnt+1
-    print()
-print("rate of concordance:", cnt/200*100, "%")
-print("o: ", cnt, "x: ",400-cnt )
+    #判定結果が正しいか
+    for j in range(SIZE):
+        for i in range(SIZE):
+            if array[i][j] == '. ':
+                decide_shortest_point(array, t_point, f_point, i,j)
+            if i >= 10:
+                if array[i][j] == out:
+                    f_cnt = f_cnt + 1
+            if i < 10:
+                if array[i][j] == ok:
+                    t_cnt = t_cnt + 1
+            #sys.stdout.write(array[i][j])
+        # print()
+    rate = (t_cnt + f_cnt) / 400 *100
+    print(rate)
+    return rate
 
+a = 0
+for i in range(10):
+    total = (execute() + total) 
+print(total/10 , '%')
