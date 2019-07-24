a9238c7a98c9b650fc35483e3ba9cd0a668c6609
riy0 <chac0ptx@gmail.com>
2019-07-24 14:51:50 +0900

    update

---
+++ basicprogramming/report/report.py
@@ -0,0 +1,145 @@
+from PIL import Image, ImageDraw
+from itertools import groupby
+
+WHITE = (255,255,255)
+
+# W: Workspace
+def find_min(W, dist):
+    u = W[0]
+    for v in W:
+        if dist[u] > dist[v]:
+            u = v
+    return u
+
+def dijkstra(A, c, x, y):
+    dist = {x: 0}               #distance
+    W = [x]                     #worklist
+    P = {}                      #precedessor
+    while W != []:
+       u = find_min(W, dist)
+       W.remove(u)
+       if u == y:
+            break
+       for v in A[u]:
+           u2,v2 = sorted([u,v])
+           dv = dist[u] + c(u2, v2)
+           if not v in dist or dv < dist[v]:
+               dist[v] = dv
+               P[v] = u
+               W.append(v)
+    if y in dist:
+        return P
+    else:
+        return None
+
+def check_adjacent():
+    cr, cg, cb, ca = im.getpixel((x+1,y))
+    if (cr, cg, cb) == WHITE:
+        adj.append((x+1, y))
+        adj_cost = tuple(sorted(((x,y),(x+1, y))))
+        if not adj_cost in C.keys():
+            C[adj_cost] = 1
+
+
+def draw_route(route, end_point):
+    temp = route[end_point[1]]
+    while end_point[0] != route[temp]:
+        adj = route[temp]
+        draw.line([adj,temp], fill=(255, 0, 0), width=4)
+        temp = adj
+
+im = Image.open("maze.png")
+draw = ImageDraw.Draw(im)
+
+(width, height) = im.size
+
+end_point =[]
+L = {}
+C = {}
+
+for x in range(0, width):
+    for y in range(0, height):
+        (r,g,b,a) = im.getpixel((x,y))
+        if (r,g,b) == (255,0,0):
+            end_point.append((x,y))
+            draw.point((x,y), fill = WHITE) 
+
+
+for x in range(0, width):
+    for y in range(0, height):
+        (r,g,b,a) = im.getpixel((x,y))
+
+        if (r,g,b) == WHITE:
+            adj = []
+            if x < width - 1:
+                cr, cg, cb, ca = im.getpixel((x+1,y))
+                if (cr, cg, cb) == WHITE:
+                    adj.append((x+1, y))
+                    adj_cost = tuple(sorted(((x,y),(x+1, y))))
+                    if not adj_cost in C.keys():
+                        C[adj_cost] = 1
+
+            if x > 0:
+                cr, cg, cb, ca = im.getpixel((x-1,y))
+                if (cr, cg, cb) == WHITE:
+                    adj.append((x-1, y))
+                    adj_cost = tuple(sorted(((x,y),(x-1, y))))
+                    if not adj_cost in C.keys():
+                        C[adj_cost] = 1
+
+            if y > 0:
+                cr, cg, cb, ca = im.getpixel((x,y-1))
+                if (cr, cg, cb) == WHITE:
+                    adj.append((x, y-1))
+                    adj_cost = tuple(sorted(((x,y),(x, y-1))))
+                    if not adj_cost in C.keys():
+                        C[adj_cost] = 1
+
+            if y < height - 1:
+                cr, cg, cb, ca = im.getpixel((x,y+1))
+                if (cr, cg, cb) == WHITE:
+                    adj.append((x, y+1))
+                    adj_cost = tuple(sorted(((x,y),(x, y+1))))
+                    if not adj_cost in C.keys():
+                        C[adj_cost] = 1
+
+            if x < width - 1 and y > 0:
+                cr, cg, cb, ca = im.getpixel((x+1,y-1))
+                if (cr, cg, cb) == WHITE:
+                    adj.append((x+1, y-1))
+                    adj_cost = tuple(sorted(((x,y),(x+1, y-1))))
+                    if not adj_cost in C.keys():
+                        C[adj_cost] = 2 ** 0.5
+
+            if x < width - 1 and y < height -1:
+                cr, cg, cb, ca = im.getpixel((x+1,y+1))
+                if (cr, cg, cb) == WHITE:
+                    adj.append((x+1, y+1))
+                    adj_cost = tuple(sorted(((x,y),(x+1, y+1))))
+                    if not adj_cost in C.keys():
+                        C[adj_cost] = 2 ** 0.5
+
+
+            if x > 0 and y < height -1:
+                cr, cg, cb, ca = im.getpixel((x-1,y+1))
+                if (cr, cg, cb) == WHITE :
+                    adj.append((x-1, y+1))
+                    adj_cost = tuple(sorted(((x,y),(x+1, y+ 1))))
+                    if not adj_cost in C.keys():
+                        C[adj_cost] = 2 ** 0.5
+
+            if x > 0 and y > 0:
+                cr, cg, cb, ca = im.getpixel((x-1,y-1))
+                if (cr, cg, cb) == WHITE :
+                    adj.append((x-1, y-1))
+                    adj_cost = tuple(sorted(((x,y),(x-1, y-1))))
+                    if not adj_cost in C.keys():
+                        C[adj_cost] = 2 ** 0.5
+
+            L[(x,y)]= adj
+
+route= dijkstra(L, lambda x,y:C[(x,y)], end_point[0], end_point[1])
+
+draw_route(route, end_point)
+
+im.save("a.png")
